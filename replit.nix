b68c88ec527849f084b082839350f7d6fc725c7c
sqt <2619519-sqt@users.noreply.replit.com>
2025-01-21 14:10:54 +0000

    Agent query: Can you see the Medical Image Viewer interface with a title and file upload button?
    Refactor image viewer to use PIL and simplify image display, removing image processing and analysis components.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/c6f213d6-1b69-444e-af54-9b52cc5ffcf6/e36806c1-a5af-449d-beab-e46edea4914b.jpg

--- replit.nix
+++ replit.nix
@@ -1,15 +1,23 @@
 {pkgs}: {
   deps = [
-    pkgs.tk
-    pkgs.tcl
     pkgs.qhull
     pkgs.pkg-config
     pkgs.gtk3
     pkgs.gobject-introspection
     pkgs.ghostscript
-    pkgs.freetype
     pkgs.ffmpeg-full
     pkgs.cairo
+    pkgs.zlib
+    pkgs.tk
+    pkgs.tcl
+    pkgs.openjpeg
+    pkgs.libxcrypt
+    pkgs.libwebp
+    pkgs.libtiff
+    pkgs.libjpeg
+    pkgs.libimagequant
+    pkgs.lcms2
+    pkgs.freetype
     pkgs.libGLU
     pkgs.libGL
   ];
